62b877f125715010d713fac4b4b768fbd4c54bd4
Devon D'Apuzzo <devon.dapuzzo@gmail.com>
2016-03-13 23:29:24 -0500

    backend working

--- webApp/index.js
+++ webApp/index.js
@@ -1,27 +1,48 @@
 var express = require('express');
 var fs = require('fs');
-var multer = require('multer');
+var http= require('http');
+// Requires multiparty
+var multiparty = require('connect-multiparty'),
+    multipartyMiddleware = multiparty();
+UserController = require('./controllers/UserController');
 var app = express();
 
 //serve frontend
 app.use(express.static(__dirname + '/public'));
 
-app.post('/upload/song', function(request, respond) {
-    var body = '';
-    var title = "/uploads/" + request.query.name;
-    filePath = __dirname + title;
-    console.log('uploading' + title);
-    request.on('data', function(data) {
-        body += data;
-    });
 
-    request.on('end', function (){
-        fs.appendFile(filePath, body, function() {
-            respond.end();
+
+// Requires controller
+var UserController = require('./controllers/UserController');
+
+// Example endpoint
+app.post('/upload/song', multipartyMiddleware,function(req, res) {
+    var file = req.files.file;
+
+    var fileName = __dirname + "/uploads/" + file.name;
+
+    console.log("BEGIN: " + fileName);
+
+
+    var fileSaveStream = fs.createReadStream(file.path).pipe(fs.createWriteStream(fileName));
+
+    fileSaveStream.on('finish', function () {
+        fs.unlink(file.path, function(err){
+            if(err){
+                console.log("error unlinking file");
+            }
         });
+        console.log("END: " + file.name);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<!doctype html><html><head><title>response</title></head><body>');
+        res.write("song: "+file.name + " uploaded");
+        res.end('</body></html>');
     });
 
-})
+
+});
+
+
 
 
 
